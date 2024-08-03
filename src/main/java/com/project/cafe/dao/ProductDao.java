f5b568667489f4bf25ed59151e6608646caf3c7c
merson21 <mersontaguiam7@gmail.com>
2024-08-03 10:10:56 +0800

    [Feature Added] - All other API of Product

--- src/main/java/com/project/cafe/dao/ProductDao.java
+++ src/main/java/com/project/cafe/dao/ProductDao.java
@@ -3,7 +3,9 @@ package com.project.cafe.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.project.cafe.POJO.Product;
 import com.project.cafe.wrapper.ProductWrapper;
@@ -12,4 +14,15 @@ public interface ProductDao extends JpaRepository<Product, Integer>{
 	
 	@Query(value = "SELECT new com.project.cafe.wrapper.ProductWrapper(p.id, p.name, p.description, p.price, p.status, p.category.id, p.category.name) FROM Product p")
 	List<ProductWrapper> getAllProduct();
+
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE product SET status = ?1 WHERE id = ?2", nativeQuery = true)
+	void updateProductStatus(String status, int id);
+
+	@Query(value ="SELECT new com.project.cafe.wrapper.ProductWrapper(p.id, p.name) FROM Product p WHERE p.category.id=:id and p.status='true'")
+	List<ProductWrapper> getProductByCategory(Integer id);
+	
+	@Query(value ="SELECT new com.project.cafe.wrapper.ProductWrapper(p.id, p.name, p.description, p.price) FROM Product p WHERE p.category.id=:id")
+	ProductWrapper getProductById(Integer id);
 }
--- src/main/java/com/project/cafe/restImpl/ProductRestImpl.java
+++ src/main/java/com/project/cafe/restImpl/ProductRestImpl.java
@@ -50,5 +50,45 @@ public class ProductRestImpl implements ProductRest{
 		}
 		return cafeUtils.getResponseEntity(cafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+
+	@Override
+	public ResponseEntity<String> deleteProduct(Integer id) {
+		try {
+			return productService.deleteProduct(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return cafeUtils.getResponseEntity(cafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@Override
+	public ResponseEntity<String> updateStatus(Map<String, String> requestMap) {
+		try {
+			return productService.updateStatus(requestMap);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return cafeUtils.getResponseEntity(cafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@Override
+	public ResponseEntity<List<ProductWrapper>> getByCategory(Integer id) {
+		try {
+			return productService.getByCategory(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@Override
+	public ResponseEntity<ProductWrapper> getProductById(Integer id) {
+		try {
+			return productService.getProductById(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ProductWrapper(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 	
 }
